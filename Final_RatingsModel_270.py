732b278dc61eced8a9ba28316780a80a02120ebc
Kronically <jacob.d.harding65@gmail.com>
2025-04-10 14:11:24 -0400

    new file:   Final_RatingsModel_270.py 	new file:   IEEE-conference-template-062824/IEEE-conference-template-062824.aux 	new file:   IEEE-conference-template-062824/IEEE-conference-template-062824.log 	new file:   IEEE-conference-template-062824/IEEE-conference-template-062824.pdf 	new file:   IEEE-conference-template-062824/IEEE-conference-template-062824.synctex.gz 	new file:   IEEE-conference-template-062824/IEEE-conference-template-062824.tex 	new file:   IEEE-conference-template-062824/IEEEtran.cls 	new file:   IEEE-conference-template-062824/IEEEtran_HOWTO.pdf 	new file:   IEEE-conference-template-062824/fig1.png 	new file:   games.csv 	new file:   games.xlsx

---
+++ Final_RatingsModel_270.py
@@ -0,0 +1,11 @@
+"""
+Final Project: Predicting Player's Ratings
+Jacob Harding, Corey Fife, Conner Francis, Aidan Manion
+
+This file houses the regression model for predicting the white and black ratings
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
